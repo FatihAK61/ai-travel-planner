f37db3b7c55c08a459aae5b479aa00fbc7964dcc
Fatih AK <63843654+FatihAK61@users.noreply.github.com>
2025-07-28 19:42:05 +0300

    Update FirebaseConfig.js

--- config/FirebaseConfig.js
+++ config/FirebaseConfig.js
@@ -4,12 +4,12 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEIzJozzP7YrOKmN_dy6m5ODlyAM670Kc",
-  authDomain: "my-travel-app-f79b9.firebaseapp.com",
-  projectId: "my-travel-app-f79b9",
-  storageBucket: "my-travel-app-f79b9.firebasestorage.app",
-  messagingSenderId: "695783264536",
-  appId: "1:695783264536:web:1a056a035058048199ce6d",
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "...",
 };
 
 export const app = initializeApp(firebaseConfig);
